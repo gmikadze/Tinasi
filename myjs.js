0c1e25815b9b8254df298fd38463f004d1eb5fe4
gmikadze <73239601+gmikadze@users.noreply.github.com>
2020-12-17 20:14:43 +0400

    carousel issue

--- myjs.js
+++ myjs.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
+<<<<<<< Updated upstream
   $('.carousel').carousel({full_width: true});
+=======
+  $('.carousel').carousel();
+  $('.carousel-slider').slider({full_width: true});
+>>>>>>> Stashed changes
 });
