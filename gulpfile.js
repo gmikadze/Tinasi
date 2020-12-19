1e90ed2456cd5d1a144e9329e13df6de98758b83
gmikadze <73239601+gmikadze@users.noreply.github.com>
2020-12-19 15:14:42 +0400

    errors fixxed

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,23 @@
 'use strict';
- 
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var uglifycss = require('gulp-uglifycss');
  
+gulp.task('css', function () {
+  gulp.src('maincss/main.css')
+    .pipe(uglifycss({
+      "uglyComments": true
+    }))
+    .pipe(gulp.dest('./dist/'));
+});
+
 sass.compiler = require('node-sass');
- 
 gulp.task('sass', function () {
   return gulp.src('scss/main.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer('last 2 versions'))
     .pipe(gulp.dest('maincss'));
 });
  
--- myjs.js
+++ myjs.js
@@ -1,8 +1,4 @@
 $(document).ready(function(){
-<<<<<<< Updated upstream
-  $('.carousel').carousel({full_width: true});
-=======
   $('.carousel').carousel();
   $('.carousel-slider').slider({full_width: true});
->>>>>>> Stashed changes
 });
