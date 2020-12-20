f2936a51fea01bb1072caff601ada8c177eb9a16
gmikadze <73239601+gmikadze@users.noreply.github.com>
2020-12-20 23:17:39 +0400

    about slider

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,8 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
 var uglifycss = require('gulp-uglifycss');
+var autoprefixer = require('gulp-autoprefixer');
  
 gulp.task('css', function () {
   gulp.src('maincss/main.css')
